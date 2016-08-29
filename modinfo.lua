efe27901cb73782f3838dcae91fa8e1659bd3423
André Bazoli <andre.bazoli@inatel.br>
2016-08-29 14:46:17 -0300

    Changed the hotkeys and added an array of possibilities for hotkeys that have more than one option

--- modinfo.lua
+++ modinfo.lua
@@ -1,11 +1,10 @@
 name = "Hotkeys"
 description = "Hotkeys to create items"
 author = "modding"
-version = "0.1"
+version = "0.3"
 forumthread = ""
 api_version = 6
 priority = 1
 dont_starve_compatible = true
 reign_of_giants_compatible = true
 shipwrecked_compatible = true
-
--- modmain.lua
+++ modmain.lua
@@ -1,88 +1,130 @@
 local handlers =
 {
-  T = 'torch',
-  
-  A = 'axe',
-  P = 'pickaxe',
-  M = 'machete',
-  S = 'shovel',
-  H = 'hammer',
-  
-  C = 'campfire',
-  O = 'coldfire',
-  
-  J = function (p, i)
-    if i and i.activeitem then
-      i:DropItem(i.activeitem, true) -- all stack
+    F1 = 'torch',
+
+    F2 = {'goldenaxe', 'axe'},
+    F3 = {'goldenpickaxe', 'pickaxe'},
+    F4 = {'goldenmachete', 'machete'},
+    F5 = {'goldenshovel', 'shovel'},
+    F6 = 'hammer',
+
+    F7 = 'campfire',
+    F8 = 'coldfire',
+
+    F9 = function (p, i)
+        if i and i.activeitem then
+            i:DropItem(i.activeitem, true) -- all stack
+        end
+    end,
+
+    F10 = function (p, i)
+        local equipped = i:GetEquippedItem(GLOBAL.EQUIPSLOTS.HANDS)
+
+        if equipped then
+            i:Unequip(GLOBAL.EQUIPSLOTS.HANDS)
+            i:GiveItem(equipped)
+        end
+    end,
+
+    F11 = function (p, i)
+        local eater = p.components.eater
+
+        if i and i.activeitem then
+            local action = GLOBAL.BufferedAction(p, i.activeitem, GLOBAL.ACTIONS.EAT)
+            p:PushBufferedAction(action)
+        end
     end
-  end,
-  
-  U = function (p, i)
-    local equipped = i:GetEquippedItem(GLOBAL.EQUIPSLOTS.HANDS)
-  
-    if equipped then
-      i:Unequip(GLOBAL.EQUIPSLOTS.HANDS)
-      i:GiveItem(equipped)
-    end
-  end,
-  
-  Y = function (p, i)
-    local eater = p.components.eater
-    
-    if i and i.activeitem then
-      local action = GLOBAL.BufferedAction(p, i.activeitem, GLOBAL.ACTIONS.EAT)
-      p:PushBufferedAction(action)
-    end
-  end
 }
 
 function ok(input)
-  return not (GLOBAL.IsPaused()
+    return (GLOBAL.IsPaused()
     or input:IsKeyDown(GLOBAL.KEY_CTRL)
     or input:IsKeyDown(GLOBAL.KEY_SHIFT)
     or input:IsKeyDown(GLOBAL.KEY_ALT))
 end
 
 for key, item in pairs(handlers) do
-  GLOBAL.TheInput:AddKeyDownHandler(GLOBAL['KEY_' .. key], function()
-    local input  = GLOBAL.TheInput
-	  local player = GLOBAL.GetPlayer()
+    GLOBAL.TheInput:AddKeyDownHandler(GLOBAL['KEY_' .. key], function()
+        local input  = GLOBAL.TheInput
+        local player = GLOBAL.GetPlayer()
 
-    local builder   = player.components.builder
-    local inventory = player.components.inventory
+        local builder   = player.components.builder
+        local inventory = player.components.inventory
+        local itemType = type(item)
 
-    if not ok(GLOBAL.TheInput) then return end
+        if ok(GLOBAL.TheInput) then return end
 
-    if type(item) == 'function' then
-      print("ACTION [ " .. key .. " ]")
-      item(player, inventory)
-    else
-      local existing = inventory:FindItem(function(e) return e.prefab == item end)
-      local recipe   = GLOBAL.GetRecipe(item)
-      
-      if existing then
-        inventory:Equip(existing)
-      elseif recipe then
-        local accessible = builder.accessible_tech_trees
-        local can_build  = builder:CanBuild(item)
-        local known      = builder:KnowsRecipe(item)
-        local prebuilt   = builder:IsBuildBuffered(item)
-        local can_do     = prebuilt or can_build and (known or GLOBAL.CanPrototypeRecipe(recipe.level, accessible))
-
-	      if recipe.placer and can_do then
-    	    builder:MakeRecipe(recipe, GLOBAL.Vector3(player.Transform:GetWorldPosition()), player:GetRotation(), function()
-            if not known then
-              player.SoundEmitter:PlaySound("dontstarve/HUD/research_unlock")
-              builder:ActivateCurrentResearchMachine()
-              builder:UnlockRecipe(item)
-            end
-          end)
+        if itemType == 'function' then
+            print("ACTION [ " .. key .. " ]")
+            item(player, inventory)
         else
-          GLOBAL.DoRecipeClick(player, recipe)
+            local existing = nil
+            local recipe = nil
+
+            if itemType == 'table' then
+                for key,specItem in ipairs(item) do
+                    existing = inventory:FindItem(function(e) return e.prefab == specItem end)
+                    if existing then
+                        print('existing')
+                        break
+                    end
+                end
+
+                for key,specItem in ipairs(item) do
+                    recipe = GLOBAL.GetRecipe(specItem)
+                    if recipe then
+                        print('Recipe')
+                        break
+                    end
+                end
+
+            else
+                existing = inventory:FindItem(function(e) return e.prefab == item end)
+                recipe   = GLOBAL.GetRecipe(item)
+            end
+
+            if existing then
+                print("ACTION [" .. key .. "]")
+                inventory:Equip(existing)
+            elseif recipe then
+                local accessible = builder.accessible_tech_trees
+                local can_build  = nil
+                local known      = nil
+                local prebuilt   = nil
+                local can_do     = nil
+
+                if itemType == 'table' then
+                    for key,specItem in ipairs(item) do
+                        can_build   = builder:CanBuild(specItem)
+                        known       = builder:KnowsRecipe(specItem)
+                        prebuilt    = builder:IsBuildBuffered(specItem)
+                        can_do      = prebuilt or can_build and (known or GLOBAL.CanPrototypeRecipe(recipe.level, accessible))
+                        if can_do then
+                            print('Can build')
+                            break
+                        end
+                    end
+                else
+                    can_build   = builder:CanBuild(item)
+                    known       = builder:KnowsRecipe(item)
+                    prebuilt    = builder:IsBuildBuffered(item)
+                    can_do      = prebuilt or can_build and (known or GLOBAL.CanPrototypeRecipe(recipe.level, accessible))
+                end
+
+                if recipe.placer and can_do then
+                    print("Doing")
+                    builder:MakeRecipe(recipe, GLOBAL.Vector3(player.Transform:GetWorldPosition()), player:GetRotation(), function()
+                        if not known then
+                            player.SoundEmitter:PlaySound("dontstarve/HUD/research_unlock")
+                            builder:ActivateCurrentResearchMachine()
+                            builder:UnlockRecipe(item)
+                        end
+                    end)
+                else
+                    GLOBAL.DoRecipeClick(player, recipe)
+                end
+
+            end
         end
-
-      end
-    end
-  end)
+    end)
 end
-
