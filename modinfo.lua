557f8f4e2ac4bf53a92ac2b184361e724325a08b
André Bazoli <andre.bazoli@inatel.br>
2016-08-29 14:43:21 -0300

    First commit - Saving the original files from the Klei forum in order to give the rights

---
+++ modinfo.lua
@@ -0,0 +1,11 @@
+name = "Hotkeys"
+description = "Hotkeys to create items"
+author = "modding"
+version = "0.1"
+forumthread = ""
+api_version = 6
+priority = 1
+dont_starve_compatible = true
+reign_of_giants_compatible = true
+shipwrecked_compatible = true
+
